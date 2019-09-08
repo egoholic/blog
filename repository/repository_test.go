3e7406ea4317184dd5809186ccf5fa6744c256e0
Vladimir Melnick <egotraumatic@gmail.com>
2019-09-09 00:39:35 +0300

    tests fix

--- repository/repository_test.go
+++ repository/repository_test.go
@@ -45,6 +45,23 @@ var _ = Describe("repository", func() {
 					Expect(publications[9].Attrs().Title).To(Equal("1th PUBLICATION"))
 				})
 			})
+
+			Context(fmt.Sprintf("when there are less than %d publications", RECENT_NUMBER), func() {
+				It(fmt.Sprintf("returns less than %d publications", RECENT_NUMBER), func() {
+					Many(RECENT_NUMBER-1, db, CreatePublication)
+					publications := repo.GetRecentPublications(context.Background())
+					Expect(publications).To(HaveLen(9))
+					Expect(publications[0].Attrs().Title).To(Equal("10th PUBLICATION"))
+					Expect(publications[1].Attrs().Title).To(Equal("9th PUBLICATION"))
+					Expect(publications[2].Attrs().Title).To(Equal("8th PUBLICATION"))
+					Expect(publications[3].Attrs().Title).To(Equal("7th PUBLICATION"))
+					Expect(publications[4].Attrs().Title).To(Equal("6th PUBLICATION"))
+					Expect(publications[5].Attrs().Title).To(Equal("5th PUBLICATION"))
+					Expect(publications[6].Attrs().Title).To(Equal("4th PUBLICATION"))
+					Expect(publications[7].Attrs().Title).To(Equal("3th PUBLICATION"))
+					Expect(publications[8].Attrs().Title).To(Equal("2th PUBLICATION"))
+				})
+			})
 		})
 		Describe(".GetAllRubrics()", func() {
 		})
